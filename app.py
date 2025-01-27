67c8179f3d5589109cd8f5b2ba5dfc10ba13d2d5
RachealN <rechealmatsikoit@gmail.com>
2025-01-27 15:29:11 +0300

    Add docker-compose.yml and Set up MySQL database connection through Docker

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask import render_template
 from app import app 
+import mysql.connector
 app = Flask(__name__, template_folder='app/templates')
+
+
+db_config = {
+    'host': 'db',  
+    'user': 'user',
+    'password': 'secret123',
+    'database': 'invoice-matching'
+}
 
 @app.route('/')
 def home():
@@ -10,6 +19,10 @@ def home():
 
 @app.route('/template')
 def template():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM invoices;") 
+    data = cursor.fetchall()
     return render_template('home.html')
 
 if __name__ == '__main__':
