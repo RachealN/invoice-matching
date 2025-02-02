eae14515c82a7b2c55e36f9e84e82c9a845ddffe
RachealN <rechealmatsikoit@gmail.com>
2025-02-03 02:20:14 +0300

    deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app.config import Config
 from app.extensions import db, migrate
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='/')
+    CORS(app)  
 
     app.debug = True
 
